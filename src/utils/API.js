b973ee63d290e476a04782ad5e85b03d3d23e71e
Eric Johnson <ericjohnson0216@icloud.com>
2020-12-23 20:54:40 -0500

    Updates

--- src/utils/API.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-let API = {
-        getRandomUsers: function(){
-        return axios.get("https://randomuser.me/api/?results=25&nat=us");
-    }
-}
-export default API
